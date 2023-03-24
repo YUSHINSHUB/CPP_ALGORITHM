c82faee042fe2211e757a337c778a9133401483a
YUSHINSHUB <kus9087@naver.com>
2023-03-24 14:48:23 +0900

    Q16693 Pizza Deal added

--- aaNotepad/aaNotepad/aaNotepad.cpp
+++ aaNotepad/aaNotepad/aaNotepad.cpp
@@ -1,11 +1,18 @@
 ﻿// aaNotepad.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
