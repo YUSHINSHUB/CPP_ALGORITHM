bd8a09af4ba290b804b992058a677cdb3a7a6b11
YUSHINSHUB <kus9087@naver.com>
2023-04-21 13:38:56 +0900

    Q1769 MULTIPLE OF 3 (IMPLEMENTATION, SILVER 5) ADDED

--- aaNotepad/aaNotepad/aaNotepad.cpp
+++ aaNotepad/aaNotepad/aaNotepad.cpp
@@ -19,45 +19,38 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int t;
-	cin >> t;
-	int res = 0;
-	int a, b;
-
-	for (int i = 0; i < t; i++) {
-		cin >> a >> b;
-		res = 0;
-
-		if (a != 0) {
-			if (a - 1 <= 0)
-				res += 5000000;
-			else if (a - 3 <= 0)
-				res += 3000000;
-			else if (a - 6 <= 0)
-				res += 2000000;
-			else if (a - 10 <= 0)
-				res += 500000;
-			else if (a - 15 <= 0)
-				res += 300000;
-			else if (a - 21 <= 0)
-				res += 100000;
-		}
-
-		if (b != 0) {
-			if (b - 1 <= 0)
-				res += 5120000;
-			else if (b - 3 <= 0)
-				res += 2560000;
-			else if (b - 7 <= 0)
-				res += 1280000;
-			else if (b - 15 <= 0)
-				res += 640000;
-			else if (b - 31 <= 0)
-				res += 320000;
-		}
-
-		cout << res << "\n";
+	int pri[1041] = {0,};
+	pri[0] = 1; // 1은 소수x 0은 소수
+	pri[1] = 0;
+	pri[2] = 0;
+	for (int i = 4; i <= 1040; i += 2) {
+		pri[i] = 1;
 	}
+
+	for (int i = 3; i <= 1040; i += 2) {
+		if (pri[i] == 0) {
+			for (int j = i * 2; j <= 1040; j += i) {
+				pri[j] = 1;
+			}
+		}
+	}
+
+	string inp;
+	cin >> inp;
+
+	int idx = 0;
+
+	for (int i = 0; i < inp.length(); i++) {
+		if (inp[i] >= 'a')
+			idx += inp[i] - 'a' + 1;
+		else
+			idx += inp[i] - 'A' + 27;
+	}
+
+	if (pri[idx] == 0)
+		cout << "It is a prime word.";
+	else
+		cout << "It is not a prime word.";
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
@@ -69,4 +62,4 @@ int main()
 //   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
 //   4. [오류 목록] 창을 사용하여 오류를 봅니다.
 //   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다
