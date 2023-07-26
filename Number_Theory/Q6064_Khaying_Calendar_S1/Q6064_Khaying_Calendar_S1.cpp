20d905c5bed6591c8b48c615a6ca126a05aa5271
YUSHINSHUB <kus9087@naver.com>
2023-07-26 19:17:52 +0900

    230726

---
+++ Number_Theory/Q6064_Khaying_Calendar_S1/Q6064_Khaying_Calendar_S1.cpp
@@ -0,0 +1,76 @@
+﻿// Q6064_Khaying_Calendar_S1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases
+	int t;
+	cin >> t;
+	int m, n, x, y;
+
+	// Loop over the test cases
+	for (int i = 0; i < t; i++) {
+		// Read the four integers for this test case
+		cin >> m >> n >> x >> y;
+
+		// Compute the maximum of m and n
+		int hi = max(m, n);
+
+		// Compute the least common multiple of m and n
+		int end = hi;
+		while (end % m != 0 || end % n != 0) {
+			end += hi;
+		}
+
+		// Determine the starting point and increment
+		int res = min(x, y);
+		int up = (res == x) ? m : n;  // if res == x then up = m else up = n
+
+		// If x or y is equal to m or n respectively, set them to zero
+		if (m == x) x = 0;
+		if (n == y) y = 0;
+
+		// Find the smallest number meeting the conditions
+		while (res <= end) {
+			if (res % m == x && res % n == y) {
+				// If the current number meets the conditions, break the loop
+				break;
+			}
+			else {
+				// Otherwise, increment res and check if we've reached the end
+				res += up;
+				if (res == end) {
+					// If we're at the end and x or y is zero, add m or n to it
+					if (x == 0) x += m;
+					if (y == 0) y += n;
+					// If res divides evenly into x and y, break the loop
+					if (res % x == 0 && res % y == 0) break;
+				}
+			}
+		}
+
+		// If no suitable number was found, output -1
+		if (res > end) res = -1;
+
+		// Output the result
+		cout << res << "\n";
+	}
+}
+
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
---
+++ Number_Theory/Q6588_Goldbachs_Conjecture_S1/Q6588_Goldbachs_Conjecture_S1.cpp
@@ -0,0 +1,68 @@
+﻿// This C++ code is designed to check and output pairs of prime numbers that sum to a given number n, 
+//as described by Goldbach's Conjecture. 
+//To do this efficiently, 
+//the program uses an Eratosthenes Sieve technique to pre-compute and mark prime numbers up to 1,000,000.
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false); // Speed up I/O operations by not syncing C and C++ I/O streams
+	cin.tie(NULL); // Untie cin from cout to speed up input operations
+	cout.tie(NULL); // Untie cout from cin to speed up output operations
+
+	// Boolean array to check if a number is prime
+	int chk[1000001];
+	chk[0] = 1; // 0 is not prime
+	chk[1] = 1; // 1 is not prime
+	for (int i = 2; i <= 1000000; i++) {
+		if (chk[i] != 1) { // If i is a prime number
+			chk[i] = 0; // Mark i as prime
+			for (int z = i * 2; z <= 1000000; z += i) {
+				chk[z] = 1; // Mark multiples of i as non-prime
+			}
+		}
+	}
+
+	while (true) {
+
+		int n;
+		cin >> n; // Read number from input
+
+		if (n == 0) { // If n is 0, break the loop
+			break;
+		}
+		bool check = true;
+
+		for (int i = 3; i <= n; i += 2) { // Start from 3 and only consider odd numbers
+			if (chk[i] == 0 && chk[n - i] == 0) { // If both i and n - i are primes
+				cout << n << " = " << i << " + " << n - i << "\n"; // Print result
+				check = false; // Mark that we have found a pair
+				break;
+			}
+		}
+
+		if (check == true) { // If no pair has been found
+			cout << "Goldbach's conjecture is wrong.\n"; // Print error message
+		}
+
+	}
+
+	return 0;
+}
+
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
