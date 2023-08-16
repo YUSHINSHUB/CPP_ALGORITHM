bd7f7828d084d08494dde3b3334b27841ee4a560
YUSIN <kus9087@naver.com>
2023-08-16 21:13:14 +0900

    230816

---
+++ Arithmetic/Q27433_FACTORIAL_2_B5/Q27433_FACTORIAL_2_B5.cpp
@@ -0,0 +1,33 @@
+﻿// Q27433_FACTORIAL_2_B5.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+/*
+Commented by ChatGPT
+Summary: This C++ program calculates the factorial of a given number.
+
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;  // Declare the integer variable n
+	cin >> n;  // Read the input number
+
+	long long res = 1;  // Declare a long long variable to store the result of the factorial
+
+	for (int i = 2; i <= n; i++)  // Loop from 2 to n
+		res *= i;  // Multiply the result by the current number
+
+	cout << res;  // Output the result
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
