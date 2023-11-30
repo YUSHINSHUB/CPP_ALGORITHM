405cb117c9bfc1fcaed18d1963aab8efeb5f5b42
USHIN KIM <kus9087@naver.com>
2023-11-30 16:01:14 +0900

    231130

---
+++ Arithmetic/Q25704_Event_B4/Q25704_Event_B4.cpp
@@ -0,0 +1,47 @@
+﻿/*
+commented by ChatGPT
+This C++ code calculates the final price after applying discounts based on the quantity of items purchased (N) and the original price (P).
+The program applies different levels of discounts for various purchase quantities: 20 items or more, 15 items or more, 10 items or more, and 5 items or more.
+The discounts include a percentage off the original price, a fixed amount off, or both. The code ensures that the final price does not go below zero.
+
+이 C++ 코드는 구매한 항목 수(N)와 원래 가격(P)에 따라 할인된 최종 가격을 계산합니다.
+프로그램은 다양한 구매 수량에 대해 다른 수준의 할인을 적용합니다: 20개 이상, 15개 이상, 10개 이상, 5개 이상.
+할인에는 원래 가격에 대한 백분율 할인, 일정 금액 할인 또는 둘 다 포함됩니다.
+이 코드는 최종 가격이 0보다 낮아지지 않도록 보장합니다.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, P; // N: 구매한 항목 수, P: 원래 가격
+	int res; // 결과 할인 가격
+
+	cin >> N >> P; // 구매한 항목 수와 원래 가격 입력 받기
+	res = P; // 할인 전 가격으로 초기화
+
+	// 할인 조건에 따라 할인된 가격 계산
+	if (N >= 20) {
+		res = min(res, (P / 4) * 3); // 25% 할인
+		res = min(res, P - 2000); // 2000원 할인
+		res = min(res, (P / 10) * 9); // 10% 할인
+	}
+	else if (N >= 15) {
+		res = min(res, P - 2000); // 2000원 할인
+		res = min(res, (P / 10) * 9); // 10% 할인
+	}
+	else if (N >= 10) {
+		res = min(res, (P / 10) * 9); // 10% 할인
+		res = min(res, P - 500); // 500원 할인
+	}
+	else if (N >= 5) {
+		res -= 500; // 500원 할인
+	}
+
+	if (res < 0) // 할인 가격이 음수가 되지 않도록 보장
+		res = 0;
+
+	cout << res; // 결과 출력
+}
---
+++ Mathematics/Q23972_Devils_Deal_B3/Q23972_Devils_Deal_B3.cpp
@@ -0,0 +1,38 @@
+﻿/*
+commented by ChatGPT
+This C++ code calculates the smallest integer res for given N and K values where res * (K - 1) is greater than or equal to N * K.
+If K is equal to 1, the code outputs -1, indicating that it is not possible to find a valid res.
+The code ensures res is the smallest possible value meeting the condition.
+It checks if (N * K) % (K - 1) is zero; if not, res is computed as ((N * K) / (K - 1)) + 1, else it is (N * K) / (K - 1).
+This approach finds the minimum number of elements needed to achieve a sum equal to or greater than N * K.
+
+이 C++ 코드는 주어진 N과 K 값에 대해 res * (K - 1)이 N * K보다 크거나 같은 가장 작은 정수 res를 계산합니다. K가 1과 같으면, 유효한 res를 찾을 수 없으므로 코드는 -1을 출력합니다.
+코드는 res가 조건을 만족하는 가능한 가장 작은 값이 되도록 합니다.
+(N * K) % (K - 1)이 0이 아닌 경우, res는 ((N * K) / (K - 1)) + 1로 계산되며, 0인 경우 res는 (N * K) / (K - 1)로 계산됩니다.
+이 방법은 N * K와 같거나 큰 합을 달성하기 위해 필요한 최소 원소 수를 찾습니다.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	long long N, K; // N과 K 변수 선언
+	long long res; // 결과값을 저장할 변수
+
+	cin >> N >> K; // 입력값을 받아 N과 K에 할당
+
+	// K가 1인 경우, 유효한 res 값을 찾을 수 없으므로 -1 출력
+	if (K == 1) {
+		cout << "-1";
+	}
+	else {
+		// res 값을 계산
+		if ((N * K) % (K - 1) != 0) // N * K가 K - 1로 나누어 떨어지지 않으면 1을 더해줌
+			res = ((N * K) / (K - 1)) + 1;
+		else // 나누어 떨어지면 그대로 계산
+			res = (N * K) / (K - 1);
+
+		cout << res; // 결과값 출력
+	}
+}
