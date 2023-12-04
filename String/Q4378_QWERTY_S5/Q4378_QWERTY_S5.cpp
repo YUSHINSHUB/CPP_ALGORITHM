790df8268f197d7c8a77f3914d7739f42127d147
USHIN KIM <kus9087@naver.com>
2023-12-04 15:54:15 +0900

    231204

---
+++ String/Q4378_QWERTY_S5/Q4378_QWERTY_S5.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s = "";
+	string change = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+	string sb = "";
+
+	while (getline(cin, s)) {
+		for (int i = 0; i < s.length(); i++) {
+			if (s[i] == ' ') {
+				sb += ' ';
+			}
+			else {
+				for (int j = 1; j < change.length(); j++) {
+					if (s[i] == change[j]) {
+						sb += change[j - 1];
+						break;
+					}
+				}
+			}
+		}
+		sb += '\n';
+	}
+
+	cout << sb;
+}
