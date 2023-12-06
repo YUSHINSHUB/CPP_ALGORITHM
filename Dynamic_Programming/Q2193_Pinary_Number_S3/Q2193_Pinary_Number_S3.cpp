a3f7a44a7ff7a58467b90c3d57f875ffa13af695
USHIN KIM <kus9087@naver.com>
2023-12-06 13:20:51 +0900

    231206

---
+++ Dynamic_Programming/Q2193_Pinary_Number_S3/Q2193_Pinary_Number_S3.cpp
@@ -0,0 +1,41 @@
+﻿/*
+commented by ChatGPT
+This C++ program calculates the number of binary strings of length N that do not contain consecutive ones, using dynamic programming.
+It employs memoization to optimize the computation of the number of such strings for each length and starting digit.
+이 C++ 프로그램은 연속된 1이 포함되지 않는 길이 N의 이진 문자열의 수를 동적 프로그래밍을 사용하여 계산합니다.
+각 길이와 시작 숫자에 대한 이러한 문자열의 수를 계산하는 데 메모이제이션을 사용하여 최적화합니다.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int N; // Length of the binary string (이진 문자열의 길이)
+long long mem1[91] = { -1, }; // Memoization array for strings starting with 1 (1로 시작하는 문자열을 위한 메모이제이션 배열)
+long long mem0[91] = { -1, }; // Memoization array for strings starting with 0 (0으로 시작하는 문자열을 위한 메모이제이션 배열)
+
+// Recursive function to calculate the number of valid strings
+static long long dp(int idx, int cur) {
+	if (cur == 1) { // If current digit is 1
+		if (mem1[idx] > 0) // If already computed, return the value (이미 계산된 경우 값을 반환)
+			return mem1[idx];
+		mem1[idx] = dp(idx + 1, 0); // Compute for the next digit being 0 (다음 자리가 0인 경우 계산)
+		return mem1[idx];
+	}
+	else { // If current digit is 0
+		if (mem0[idx] > 0) // If already computed, return the value (이미 계산된 경우 값을 반환)
+			return mem0[idx];
+		// Compute for the next digit being 1 and 0 (다음 자리가 1과 0인 경우 계산)
+		mem0[idx] = dp(idx + 1, 1) + dp(idx + 1, 0);
+		return mem0[idx];
+	}
+}
+
+
+int main() {
+	cin >> N; // Read the length of the binary string (이진 문자열의 길이 읽기)
+	mem1[N] = 1; // Base case: One valid string of length N starting with 1 (기본 사례: 1로 시작하는 길이 N의 유효한 문자열 하나)
+	mem0[N] = 1; // Base case: One valid string of length N starting with 0 (기본 사례: 0으로 시작하는 길이 N의 유효한 문자열 하나)
+
+	cout << dp(1, 1); // Output the result (결과 출력)
+}
---
+++ Implementation/Q1244_Turn_On_and_Off_S4/Q1244_Turn_On_and_Off_S4.cpp
@@ -0,0 +1,56 @@
+﻿/*
+commented by ChatGPT
+This C++ program reads an array of switches and a series of operations to toggle them.
+It supports two types of operations: toggling every k-th switch and toggling a symmetrical range of switches centered at a given index.
+The final state of the switches is printed, with a line break after every 20 switches.
+이 C++ 프로그램은 스위치 배열과 그것들을 토글하는 일련의 연산을 읽습니다.
+두 가지 유형의 연산을 지원합니다: k번째 스위치를 토글하는 것과 주어진 인덱스를 중심으로 대칭 범위의 스위치를 토글하는 것.
+최종 스위치 상태는 매 20개 스위치마다 줄 바꿈과 함께 출력됩니다.
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N, T; // N: Number of switches, T: Number of operations (N: 스위치 수, T: 연산 수)
+
+	cin >> N;
+	vector<int> sch(N + 1); // Vector to store the state of switches (스위치 상태를 저장할 벡터)
+	for (int i = 1; i <= N; i++) {
+		cin >> sch[i]; // Read initial state of switches (스위치의 초기 상태 읽기)
+	}
+	cin >> T; // Read number of operations (연산 수 읽기)
+
+	for (int i = 0; i < T; i++) {
+		int gen, num; // gen: gender (1 or 2), num: switch number (gen: 성별 (1 또는 2), num: 스위치 번호)
+		cin >> gen >> num;
+		if (gen == 1) { // Operation type 1: Toggle every k-th switch (연산 유형 1: k번째 스위치 토글)
+			for (int j = num; j <= N; j += num) {
+				sch[j] = sch[j] == 1 ? 0 : 1; // Toggle the switch (스위치 토글)
+			}
+		}
+		else { // Operation type 2: Toggle a symmetrical range of switches (연산 유형 2: 대칭 범위의 스위치 토글)
+			int low = num;
+			int hig = num;
+			// Expand the symmetrical range (대칭 범위 확장)
+			while (low > 1 && hig < N && sch[low - 1] == sch[hig + 1]) {
+				low--;
+				hig++;
+			}
+			// Toggle switches in the range (범위 내의 스위치 토글)
+			for (int j = low; j <= hig; j++) {
+				sch[j] = sch[j] == 1 ? 0 : 1;
+			}
+		}
+	}
+
+	// Output the final state of switches (스위치의 최종 상태 출력)
+	for (int i = 1; i <= N; i++) {
+		cout << sch[i] << " ";
+		if (i % 20 == 0) // Line break after every 20 switches (20개 스위치마다 줄 바꿈)
+			cout << "\n";
+	}
+}
