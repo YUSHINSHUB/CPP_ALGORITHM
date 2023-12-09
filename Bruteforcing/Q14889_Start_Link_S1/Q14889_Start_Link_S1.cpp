04eeddda7c9143360f6cb1d81385e194265c2992
USHIN KIM <kus9087@naver.com>
2023-12-09 17:14:56 +0900

    231209

---
+++ Bruteforcing/Q14889_Start_Link_S1/Q14889_Start_Link_S1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <math.h>
+#include <climits>
+
+using namespace std;
+
+static int n;
+static int res = INT_MAX;
+static int par[20][20];
+static bool visited[20];
+
+static void bt(int idx, int cnt) {
+	if (cnt == n / 2) {
+		int s = 0, l = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (visited[i] && visited[j])
+					s += par[j][i];
+				else if (!visited[i] && !visited[j])
+					l += par[j][i];
+			}
+		}
+		res = min(res, abs(s - l));
+	}
+	else {
+		for (int i = idx; i < n; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				bt(i + 1, cnt + 1);
+				visited[i] = false;
+			}
+		}
+	}
+}
+
+
+int main()
+{
+
+	cin >> n;
+	fill(visited, visited + n, false);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			cin >> par[i][j];
+	}
+
+	bt(0, 0);
+	cout << res;
+
+}
